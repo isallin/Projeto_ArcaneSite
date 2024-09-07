56fe4dc2fd8dd057e28a31655c56eba1f9f8de52
isalin <isa@teste.com>
2024-09-07 14:31:13 -0300

    Carousel em progresso

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(){ 
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+
+        // If we need pagination
+
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+});
