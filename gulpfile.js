2336620ba4e10e2a261018264ae1a8b8ebf19ec6
isalin <isa@teste.com>
2024-09-08 18:48:03 -0300

    Apagando arquivo e minificando o JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function images(){
     return gulp.src('./src/images/*')
@@ -14,7 +21,8 @@ function styles(){
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = gulp.parallel(styles,images);
+exports.default = gulp.parallel(styles,images,scripts);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./scr/scripts/*.js', gulp.parallel(scripts))
 }
