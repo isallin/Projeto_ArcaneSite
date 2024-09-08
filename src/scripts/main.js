7d00e52ab06eae0e49646acbed7248d0974f1f55
isalin <isa@teste.com>
2024-09-08 18:35:45 -0300

    Ajustes finais

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,51 @@
 document.addEventListener('DOMContentLoaded', function(){ 
-    const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        direction: 'vertical',
-        loop: true,
+    const swiper = new Swiper('.carousel__wrapper', {
+        spaceBetween: 30,
 
-        // If we need pagination
-
-
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+        pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
         },
+
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+
+        breakpoints:{
+            0:{
+                slidesPerView:1
+            },
+            768:{
+                slidesPerView:3
+            },
+            1023:{
+                slidesPerView:5
+            }
+        }
+
     });
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual<alturaHero){
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+    function ocultaElementosDoHeader(){
+        const header = document.querySelector('header');
+        header.classList.add('header--is-hidden');
+    }
+    
+    function exibeElementosDoHeader(){
+        const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden');
+    }
 
 });
